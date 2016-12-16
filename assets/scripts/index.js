57272dc0eb28002ef15c1bee63c4a161ccb6ee29
Bowers96 <bowersc96@gmail.com>
2016-12-16 15:53:45 -0500

    Add show and hide function for multiple page views
    -User can Sign up and be directed to Sign in
    -User can Sign in and be directed to launch page
    -User can Change Password and be directed back to launch page
    -User can Sign Out and be directed to Sign Out view

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -2,10 +2,53 @@
 require('../styles/index.scss');
 const authEvents = require('./auth/events.js');
 
+// Allows user to click icon to open sidebar
 
 $(document).ready(function() {
   $('#sidebar-btn').click(function() {
     $('#sidebar').toggleClass('visible');
   });
   authEvents.addHandlers();
+});
+
+// Changes page views depending on what authentication link is clicked
+
+$("#auth-one").click(function() {
+  $('.frame').hide();
+  $('.sign-up').show();
+  $('#sidebar').toggleClass('visible');
+});
+
+$("#btn-one").click(function() {
+  $('.frame').hide();
+  $('.sign-in').show();
+
+});
+
+$("#auth-two").click(function() {
+  $('.frame').hide();
+  $('.sign-in').show();
+  $('#sidebar').toggleClass('visible');
+});
+
+$("#btn-two").click(function() {
+  $('.frame').hide();
+  $('body').show();
+});
+
+$("#auth-three").click(function() {
+  $('.frame').hide();
+  $('.change-password').show();
+  $('#sidebar').toggleClass('visible');
+});
+
+$("#btn-three").click(function() {
+  $('.frame').hide();
+  $('body').show();
+});
+
+$("#auth-four").click(function() {
+  $('.frame').hide();
+  $('.sign-out').show();
+  $('#sidebar').toggleClass('visible');
 });
