90681f0085dd43b99e9e5ca598400edfa09d00a2
kfili <kosta.filiotis@gmail.com>
2016-12-16 11:37:58 -0500

    Add auth files from previous project.
    - Also Modified them for use with an Express backend, as they were
      previously used with rails.

---
+++ assets/scripts/auth/api.js
@@ -0,0 +1,99 @@
+'use strict';
+
+const app = require('../app');
+
+const store = require('../store');
+// const getFormFields = require('../../../lib/get-form-fields.js');
+
+//authApi.signUp(authUi.success, authUi.failure, data);
+
+const signUp = function(data){
+  return $.ajax({
+    url: app.host + '/sign-up/',
+    method: 'POST',
+    data,
+  });
+};
+
+const signIn = function(data){
+  return $.ajax({
+    url: app.host + '/sign-in/',
+    method: 'POST',
+    data,
+  });
+};
+
+const signOut = function(){
+  return $.ajax({
+    method: 'DELETE',
+    url: app.host + '/sign-out/' + store.user._id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+    },
+  });
+};
+
+const changePassword = function(data){
+  return $.ajax({
+    method: 'PATCH',
+    url: app.host + '/change-password/' + store.user._id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+    },
+    data: data,
+  });
+};
+
+const createPlace = function(data){
+  return $.ajax({
+    url: app.host + '/places/',
+    method: 'POST',
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+    },
+    data,
+  });
+};
+
+const updatePlace = function(data, id){
+  return $.ajax({
+    method: 'PATCH',
+    url: app.host + '/places/' + id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+    },
+    data: data,
+  });
+};
+
+
+const showPlaces = function(){
+  return $.ajax({
+    url: app.host + '/places',
+    method: 'GET',
+    headers : {
+        Authorization: 'Token token=' + store.user.token,
+    }
+  });
+};
+
+const deletePlace = function(id){
+  return $.ajax({
+    method: 'DELETE',
+    url: app.host + '/places/' + id,
+    headers: {
+      Authorization: 'Token token=' + store.user.token,
+    },
+  });
+};
+
+module.exports = {
+  signUp,
+  signIn,
+  signOut,
+  changePassword,
+  createPlace,
+  updatePlace,
+  showPlaces,
+  deletePlace
+};
