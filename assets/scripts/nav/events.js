57de78f1f8bf9a9804b2d8924f149d1da4dac799
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-19 09:27:32 -0500

    Add comments to events.js.
    Add comments to events.js to clarify purpose.

--- assets/scripts/nav/events.js
+++ assets/scripts/nav/events.js
@@ -1,37 +1,44 @@
 'use strict';
 
+// Open the side bar
 const onSideBarBtn = function() {
   $('#sidebar').toggleClass('visible');
 };
 
+// Sign up link from nav bar
 const onSignUpLink = function() {
   $('.frame').hide();
   $('.sign-up').show();
   $('#sidebar').toggleClass('visible');
 };
 
+// Sign in link from nav bar
 const onSignInLink = function() {
   $('.frame').hide();
   $('.sign-in').show();
   $('#sidebar').toggleClass('visible');
 };
 
+// Sign in submit button
 const onBtnTwoLink = function() {
   $('.frame').hide();
   $('body').show();
 };
 
+// Change PW on nav bar
 const onChangePasswordLink = function() {
   $('.frame').hide();
   $('.change-password').show();
   $('#sidebar').toggleClass('visible');
 };
 
+// Change PW submit button
 const onBtnThreeLink = function() {
   $('.frame').hide();
   $('body').show();
 };
 
+// Sign out on nav bar and redirects to signout view/message
 const onSignOutLink = function() {
   $('.frame').hide();
   $('.sign-out').show();
