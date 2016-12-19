4bfa23f84be2fc7edfddbb994d9c0c9b4008909f
“coffeeisamazing” <caleb.searles@libertymutual.com>
2016-12-19 10:04:11 -0500

    Fix auth errors.
    * Merged events into ui methods.

--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -5,19 +5,46 @@ const launchTemplate = require('../templates/launch.handlebars');
 
 const signInSuccess = (data) => {
   store.user = data.user;
+  $('.frame').hide();
+  $('body').show();
+  // $('form').find('input').val('');
+  $('.launch').show();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
   $('#content').html(launchTemplate());
 };
 
 const signOutSuccess = () => {
   $("#messages").text("sign out success");
+  $('.frame').hide();
+  $('.sign-out').show();
+  $('#sidebar').toggleClass('visible');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 };
 
 const changePasswordSuccess = () => {
   $("#messages").text("password changed");
+  $('.frame').hide();
+  $('body').show();
+  // $('form').find('input').val('');
+  $('.launch').show();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 };
 
 const signUpSuccess = () => {
   $("#messages").text("sign up success");
+  $('.frame').hide();
+  $('.sign-in').show();
+  // $('form').find('input').val('');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
 };
 
 const failure = (error) => {
