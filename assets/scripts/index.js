2abe1a38f2b66f80ac9ab3f46d5cb7c43094c074
Bowers96 <bowersc96@gmail.com>
2016-12-18 21:43:11 -0500

    Polish jQuery event handlers
    -Showing and hiding appropriate views using jQuery

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -17,41 +17,85 @@ $("#sign-up-nav").click(function() {
   $('.frame').hide();
   $('.sign-up').show();
   $('#sidebar').toggleClass('visible');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
 });
 
-// $("#btn-one").click(function() {
-//   $('.frame').hide();
-//   $('.sign-in').show();
-//
-// });
+$("#btn-one").click(function() {
+  $('.frame').hide();
+  $('.sign-in').show();
+  $('form').find('input').val('');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+});
 
 $("#sign-in-nav").click(function() {
   $('.frame').hide();
   $('.sign-in').show();
   $('#sidebar').toggleClass('visible');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
 });
 
 $("#btn-two").click(function() {
   $('.frame').hide();
   $('body').show();
+  $('form').find('input').val('');
+  $('.launch').show();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 });
 
 $("#change-password-nav").click(function() {
   $('.frame').hide();
   $('.change-password').show();
   $('#sidebar').toggleClass('visible');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 });
 
 $("#btn-three").click(function() {
   $('.frame').hide();
   $('body').show();
+  $('form').find('input').val('');
+  $('.launch').show();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 });
 
 $("#sign-out-nav").click(function() {
   $('.frame').hide();
   $('.sign-out').show();
   $('#sidebar').toggleClass('visible');
+  $('.launch').hide();
+  $('.file-table-one').hide();
+  $('.file-table-two').hide();
+  $('.description').hide();
 });
+
+// Launch page button views of data tables
+
+$(".one").click(function() {
+  $('.launch').hide();
+  $('.file-table-one').show();
+  $('.file-table-two').hide();
+  $('.description').hide();
+});
+
+$(".two").click(function() {
+  $('.launch').hide();
+  $('.file-table-two').show();
+  $('.file-table-one').hide();
+  $('.description').hide();
+});
+
 
 $('form').on('submit', function(event) {
   console.log('hey there you made it', this); // The form being submitted.
