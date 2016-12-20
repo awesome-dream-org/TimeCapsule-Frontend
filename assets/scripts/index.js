98b54a75f550d2b9fc889f4a65cb76b7f0b14902
kibs86 <kibs086@gmail.com>
2016-12-19 19:43:46 -0500

    Have navbar open on title page load
    - The navbar opens by default on title page load so users can see
      the sign-in and sign-up buttons (avoids confusion)

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -14,6 +14,7 @@ $(() => {
   $('#change-password-nav').hide();
   $('#sign-out-nav').hide();
   $('#launch-page-nav').hide();
+  $('#sidebar').toggleClass('visible');
 });
 
 // Go to manage my files
