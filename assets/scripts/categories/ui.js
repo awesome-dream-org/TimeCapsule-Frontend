2a115b0a13856fbe932121cb9f3e840a709b1302
kibs86 <kibs086@gmail.com>
2016-12-19 20:28:14 -0500

    Sort categories dropdown alphabetically
    - Update the category dropdown on upload file to sort alphabetically

--- assets/scripts/categories/ui.js
+++ assets/scripts/categories/ui.js
@@ -2,13 +2,30 @@
 
 const msg = require('../common/user-messages.js');
 
+const sortCategoryList = function(categories) {
+  return categories.sort(function(a, b) {
+    let textA = a.text.toUpperCase(); // ignore upper and lowercase
+    let textB = b.text.toUpperCase(); // ignore upper and lowercase
+    if (textA < textB) {
+      return -1;
+    }
+    if (textA > textB) {
+      return 1;
+    }
+
+    // names must be equal
+    return 0;
+  });
+};
+
 const updateCategorySelect = function(data) {
+  let categoryList = sortCategoryList(data.categories);
   let list = $('#select-category')[0];
-  for (let i = 0; i < data.categories.length; i++) {
+  for (let i = 0; i < categoryList.length; i++) {
     list.options[list.options.length] =
       new Option(
-        data.categories[i].text,
-        data.categories[i].id);
+        categoryList[i].text,
+        categoryList[i].id);
   }
   $('#select-category').append(list);
 };
